bb80dd5e8a35163d26a3c18e226caccc91d00ff0
Mike Timm <mtimm@tetrationanalytics.com>
2023-09-14 12:56:51 -0700

    Add almalinux support

--- bigtop_toolchain/manifests/gnupg.pp
+++ bigtop_toolchain/manifests/gnupg.pp
@@ -16,7 +16,7 @@
 class bigtop_toolchain::gnupg {
 
   case $operatingsystem {
-    /(?i:(centos|fedora|redhat|rocky))/: {
+    /(?i:(almalinux|centos|fedora|redhat|rocky))/: {
        $pkg = "gnupg2"
        $cmd = "gpg2"
     }
